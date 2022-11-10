b9731a745ec366d6f58aa0add97f60e5527aa2b9
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2022-11-09 21:25:15 -0500

    Added functionality for searchAPI

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const wrapper = 
+// const wrapper = 
 
 
 
@@ -12,16 +12,19 @@ const wrapper =
 
 
 
+let baseURL = "https://developer.musixmatch.com/";
 
+// Function for JSON conversion
+async function convertToJson (response) {
+    return await response.json();
+}
 
-
-
-
-
-
-
-
-
-
-
-
+// Function that gets the searched artist query URL
+// Returns the data in JSON format
+async function searchSongAPI (artist) {
+    let requestURL = `${baseURL}artist.search?q_artist=${artist}&q_track=${track}`;
+    let response = await fetch(requestURL);
+    let data = await convertToJson(response);
+    console.log(data);
+    return data;
+}
