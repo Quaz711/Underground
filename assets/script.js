128bbcb09e6d17c3ca43f20efca38c6e5cc98a56
Cherry Ella <111384784+CherryElla@users.noreply.github.com>
2022-11-15 16:13:04 -0500

    worked on fonts and the headers- added strobe effect

--- assets/script.js
+++ assets/script.js
@@ -405,3 +405,4 @@ $getRandomOffset = rand(1, 1000);
 //This is the url that gets the results out of the Spotify API. You have to put in the variables you created above.
 $url = "https://api.spotify.com/v1/search?query=$getRandomSongs&offset=$getRandomOffset&limit=1&type=track&market=NL";
 */
+
